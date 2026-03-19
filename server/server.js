7b6c67f020ad50d96aaa1d0a79bf47818ab0b24d
maxpasegroup-debug <maxpasegroup@gmail.com>
2026-03-19 14:59:58 +0530

    api fix

--- server/server.js
+++ server/server.js
@@ -2,17 +2,14 @@ const express = require("express");
 
 const app = express();
 
-// Root check
 app.get("/", (req, res) => {
   res.send("Backend LIVE 🚀");
 });
 
-// API test
 app.get("/api/test", (req, res) => {
   res.json({ message: "API working" });
 });
 
-// IMPORTANT for Railway
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
